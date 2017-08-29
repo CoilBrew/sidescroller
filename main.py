2e1b53a37ae517d9d1a240541802bb9216dcf84c
Sam Edwards <samuel.j.o.edwards@gmail.com>
2017-08-29 18:48:04 +0100

    Moved main.py, added display and started while loop

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pygame
+
+
+pygame.init()
+
+info = pygame.display.Info()
+
+width = info.current_w
+height = int((info.current_h / 2))
+
+print(int(6.7/2))
+
+print ((width,height))
+print (width,height)
+
+display = pygame.display.set_mode((width, height))
+
+while True:
+    pygame.display.update()
