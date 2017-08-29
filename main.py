8bad491ae47bb67552cf1996dc8561830ee8faa8
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-08-29 20:31:38 +0100

    Added event logic to quit and scaffolding for Floor

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
-
-import pygame
+import pygame, sys
 import src.Floor
+from pygame.locals import *
 
 pygame.init()
-
 info = pygame.display.Info()
-
-width = info.current_w
-height = int((info.current_h / 2))
-
-print(int(6.7/2))
-
-print ((width,height))
-print (width,height)
+width = info.current_w # Width is the width of the screen
+height = int((info.current_h / 2)) # Height is the screen height divided byß 2
 
 display = pygame.display.set_mode((width, height))
 
+FPS = 60
+
+# Game loop
 while True:
+    # Event loop
+    for e in pygame.event.get():
+        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
+
     pygame.display.update()
+    pygame.time.Clock().tick(FPS)
---
+++ src/Floor.py
@@ -0,0 +1,10 @@
+class Floor(object):
+    """This class defines the floor"""
+    def __init__(self, display):
+        self.display = display # Takes a display argument and stores it internally
+        self.colour = "Fill me in" # RBG colours: i.e. (0, 0, 0) = black :: (Int, Int, Int)
+        self.endpoint = "Fill me in" # The endpoint of the floor, where the level ends :: (Int, Int)
+
+    def segments(self):
+        """Segments is a list of segments"""
+        pass
