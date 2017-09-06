a3c164ea78d76f3c141de1b69cdc965c2eff7538
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-09-06 12:49:24 +0100

    Wrapped game in main call

--- main.py
+++ main.py
@@ -3,30 +3,34 @@ import pygame, sys, json
 #import src.Floor
 from pygame.locals import *
 
-pygame.init()
-INFO = pygame.display.Info()
+def main():
+    pygame.init()
+    INFO = pygame.display.Info()
 
-# Settings
-settings = json.load(open("./settings.json"))
-FPS = settings["FPS"]
+    # Settings
+    settings = json.load(open("./settings.json"))
+    FPS = settings["FPS"]
 
-# Set the resolution settings
-if settings["resolution"] == "dynamic":
-    WIDTH = INFO.current_w # Width is the width of the screen
-    HEIGHT = int((INFO.current_h / 2)) # Height is the screen height divided by 2
-else:
-    WIDTH = settings["resolution"]["width"] # Width is the width of the screen
-    HEIGHT = settings["resolution"]["height"] # Height is the screen height divided by 2
+    # Set the resolution settings
+    if settings["resolution"] == "dynamic":
+        WIDTH = INFO.current_w # Width is the width of the screen
+        HEIGHT = int((INFO.current_h / 2)) # Height is the screen height divided by 2
+    else:
+        WIDTH = settings["resolution"]["width"] # Width is the width of the screen
+        HEIGHT = settings["resolution"]["height"] # Height is the screen height divided by 2
 
-display = pygame.display.set_mode((WIDTH, HEIGHT))
+    display = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Game loop
-while True:
-    # Event loop
-    for e in pygame.event.get():
-        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
+    # Game loop
+    while True:
+        # Event loop
+        for e in pygame.event.get():
+            if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
 
-    pygame.display.update()
-    pygame.time.Clock().tick(FPS)
+        pygame.display.update()
+        pygame.time.Clock().tick(FPS)
+
+if __name__ == "__main__":
+    main()
