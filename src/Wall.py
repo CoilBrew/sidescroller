6e518d96a90676473cf9244d8df2384774603529
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-09-19 20:16:09 +0100

    Started wall

---
+++ src/Wall.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Wall(pygame.sprite.Sprite):
+    """This defines the wall"""
+    def __init__(self, display):
+        """
+        abs_pos is position not relative to camera, but entire universe
+        velocity is the rate at which the wall moves to the right, in absolute terms
+        """
+        self.abs_pos = 0
+        self.velocity = 5
+
+    def move(self):
+        """Update the wall's absolute position"""
+        self.abs_pos += self.velocity
