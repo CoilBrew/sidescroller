cc119ba10a322f23f3799b29ecefe6e923f85023
Samuel Edwards <sam.edwards@themarketinglab.co.uk>
2017-09-25 22:39:40 +0100

    Edited Floor to inherit from Universe class (I think!)

--- src/Floor.py
+++ src/Floor.py
@@ -1,9 +1,13 @@
-class Floor(object):
+
+
+class Floor(Universe):
     """This class defines the floor"""
-    def __init__(self, display):
+    def __init__(self, display, level):
+        Universe.__init__(self, level)
         self.display = display # Takes a display argument and stores it internally
         self.colour = "Fill me in" # RBG colours: i.e. (0, 0, 0) = black :: (Int, Int, Int)
-        self.endpoint = "Fill me in" # The endpoint of the floor, where the level ends :: (Int, Int)
+        self.startpoint = 0
+        self.endpoint = self.levellength #inheritesd from the Universe class?    # The endpoint of the floor, where the level ends :: (Int, Int)
 
     def segments(self):
         """Segments is a list of segments"""
