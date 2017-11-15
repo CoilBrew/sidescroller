ec22e6edb748d077910465df54bd33a0832a4bf0
chris-bacon <chris.bacon@geekology.ltd.uk>
2017-11-15 08:06:00 +0000

    Started timer class

--- src/Timer.py
+++ src/Timer.py
@@ -8,7 +8,7 @@ class Timer(object):
 
     def render(self, x, y):
         fontObj = pygame.font.Font('freesansbold.ttf', 32)
-        timer = fontObj.render("Timer", True, self.fontColour, self.bg) # TODO dynamic text
+        timer = fontObj.render("Timer: <time here>", True, self.fontColour, self.bg) # TODO dynamic text
         timer_rect = timer.get_rect(center = (x, y))
         self.display.blit(timer, timer_rect)
 
